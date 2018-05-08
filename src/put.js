b8daf6ab9ce9f370dcafb4868ca7f9b945853a9a
Mark Nadal <mark@accelsor.com>
2018-05-08 16:40:55 -0700

    mimeograph fix

--- src/put.js
+++ src/put.js
@@ -1,10 +1,10 @@
 
 var Gun = require('./root');
 Gun.chain.put = function(data, cb, as){
-	// #soul.field=value>state
+	// #soul.has=value>state
 	// ~who#where.where=what>when@was
 	// TODO: BUG! Put probably cannot handle plural chains!
-	var gun = this, at = (gun._), root = at.root, tmp;
+	var gun = this, at = (gun._), root = at.root.gun, tmp;
 	as = as || {};
 	as.data = data;
 	as.gun = as.gun || gun;
@@ -36,14 +36,15 @@ Gun.chain.put = function(data, cb, as){
 		return gun;
 	}
 	if(Gun.is(data)){
-		data.get(function(at,ev){ev.off();
-			var s = Gun.node.soul(at.put);
-			if(!s){Gun.log("The reference you are saving is a", typeof at.put, '"'+ as.put +'", not a node (object)!');return}
-			gun.put(Gun.val.rel.ify(s), cb, as);
+		data.get('_').get(function(at, ev, tmp){ ev.off();
+			if(!(tmp = at.gun) || !(tmp = tmp._.back) || !tmp.soul){
+				return Gun.log("The reference you are saving is a", typeof at.put, '"'+ as.put +'", not a node (object)!');
+			}
+			gun.put(Gun.val.rel.ify(tmp.soul), cb, as);
 		});
 		return gun;
 	}
-	as.ref = as.ref || (root === (tmp = at.back))? gun : tmp;
+	as.ref = as.ref || (root._ === (tmp = at.back))? gun : tmp.gun;
 	if(as.ref._.soul && Gun.val.is(as.data) && at.get){
 		as.data = obj_put({}, at.get, as.data);
 		as.ref.put(as.data, as.soul, as);
@@ -89,21 +90,34 @@ function stun(cb){
 
 function batch(){ var as = this;
 	if(!as.graph || obj_map(as.stun, no)){ return }
-	(as.res||iife)(function(){
+	as.res = as.res || function(cb){ if(cb){ cb() } };
+	as.res(function(){
 		var cat = (as.gun.back(-1)._), ask = cat.ask(function(ack){
 			this.off(); // One response is good enough for us currently. Later we may want to adjust this.
 			if(!as.ack){ return }
 			as.ack(ack, this);
 		}, as.opt);
+		// NOW is a hack to get synchronous replies to correctly call.
+		// and STOP is a hack to get async behavior to correctly call.
+		// neither of these are ideal, need to be fixed without hacks,
+		// but for now, this works for current tests. :/
+		var tmp = cat.root.now; obj.del(cat.root, 'now'); cat.root.PUT = true;
+		var tmp2 = cat.root.stop;
+		(as.ref._).now = true;
 		(as.ref._).on('out', {
 			gun: as.ref, put: as.out = as.env.graph, opt: as.opt, '#': ask
 		});
+		obj.del((as.ref._), 'now');
+		obj.del((cat.root), 'PUT');
+		cat.root.now = tmp;
+		cat.root.stop = tmp2;
 	}, as);
 	if(as.res){ as.res() }
-} function no(v,f){ if(v){ return true } }
+} function no(v,k){ if(v){ return true } }
 
-function map(v,f,n, at){ var as = this;
-	if(f || !at.path.length){ return }
+function map(v,k,n, at){ var as = this;
+	//if(Gun.is(v)){} // TODO: HANDLE!
+	if(k || !at.path.length){ return }
 	(as.res||iife)(function(){
 		var path = at.path, ref = as.ref, opt = as.opt;
 		var i = 0, l = path.length;
@@ -132,20 +146,22 @@ function map(v,f,n, at){ var as = this;
 	}, {as: as, at: at});
 }
 
-function soul(at, ev){ var as = this.as, cat = as.at; as = as.as;
+function soul(msg, ev){ var as = this.as, cat = as.at; as = as.as;
 	//ev.stun(); // TODO: BUG!?
-	if(!at.gun || !at.gun._.back){ return } // TODO: Handle
+	if(!msg.gun || !msg.gun._.back){ return } // TODO: Handle
+	var at = msg.gun._, at_ = at;
+	var _id = (msg.put||empty)['#'];
 	ev.off();
-	at = (at.gun._.back._); // go up 1!
-	var id = id || Gun.node.soul(cat.obj) || Gun.node.soul(at.put) || Gun.val.rel.is(at.put) || (as.gun.back('opt.uuid') || Gun.text.random)(); // TODO: BUG!? Do we really want the soul of the object given to us? Could that be dangerous?
+	at = (msg.gun._.back); // go up 1!
+	var id = id || Gun.node.soul(cat.obj) || Gun.node.soul(at.put) || Gun.val.rel.is(at.put) || _id || at_._id || (as.gun.back('opt.uuid') || Gun.text.random)(); // TODO: BUG!? Do we really want the soul of the object given to us? Could that be dangerous?
 	if(!id){ // polyfill async uuid for SEA
 		at.gun.back('opt.uuid')(function(err, id){ // TODO: improve perf without anonymous callback
 			if(err){ return Gun.log(err) } // TODO: Handle error.
-			solve(at, id, cat, as);
+			solve(at, at_._id = at_._id || id, cat, as);
 		});
 		return;
 	}
-	solve(at, id, cat, as);
+	solve(at, at_._id = at_._id || id, cat, as);
 }
 
 function solve(at, id, cat, as){
@@ -162,7 +178,8 @@ function any(at, ev){
 		console.log("Please report this as an issue! Put.any.err");
 		return;
 	}
-	var cat = (at.gun._.back._), data = cat.put, opt = as.opt||{}, root, tmp;
+	var cat = (at.gun._.back), data = cat.put, opt = as.opt||{}, root, tmp;
+	if((tmp = as.ref) && tmp._.now){ return }
 	ev.off();
 	if(as.ref !== as.gun){
 		tmp = (as.gun._).get || cat.get;
@@ -182,19 +199,19 @@ function any(at, ev){
 			});
 		}
 		tmp = tmp || cat.get;
-		cat = (cat.root.get(tmp)._);
+		cat = (cat.root.gun.get(tmp)._);
 		as.not = as.soul = tmp;
 		data = as.data;
 	}
 	if(!as.not && !(as.soul = Gun.node.soul(data))){
 		if(as.path && obj_is(as.data)){ // Apparently necessary
-			as.soul = (opt.uuid || cat.root._.opt.uuid || Gun.text.random)();
+			as.soul = (opt.uuid || cat.root.opt.uuid || Gun.text.random)();
 		} else {
 			//as.data = obj_put({}, as.gun._.get, as.data);
 			if(node_ == at.get){
-				as.soul = (at.put||empty)['#'];
+				as.soul = (at.put||empty)['#'] || at._id;
 			}
-			as.soul = as.soul || at.soul || cat.soul || (opt.uuid || cat.root._.opt.uuid || Gun.text.random)();
+			as.soul = as.soul || at.soul || cat.soul || (opt.uuid || cat.root.opt.uuid || Gun.text.random)();
 		}
 		if(!as.soul){ // polyfill async uuid for SEA
 			as.ref.back('opt.uuid')(function(err, soul){ // TODO: improve perf without anonymous callback
