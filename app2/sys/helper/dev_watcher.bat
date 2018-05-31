db1edc773fc4e94c89196df9fbc3c18bd530c15e
Robert Heessels <robert@heessels.com>
2018-05-31 12:32:38 +0200

    Removed old app.

--- app2/sys/helper/dev_watcher.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-echo:
-echo =================================================
-echo Watching file changes so the box will be updated.
-echo (You can ignore initial error(s)).
-echo =================================================
-echo:
-"C:/Program Files/watchexec/watchexec.exe" %1
