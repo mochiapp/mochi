9d71e2c0f55a41d1f224685ee9112b7e1ffafcdb
Jonathan Frazier <jjfraz11@gmail.com>
2018-06-09 12:55:19 -0700

    Only create server as http when creds found.

--- server/src/index.js
+++ server/src/index.js
@@ -25,18 +25,16 @@ function createServer(app) {
   const serverPort = app.get('port')
 
   let options
-  if ( process.env.NODE_ENV !== 'production' && false ){
-    const { PRIVKEY_PATH, CERT_PATH } = process.env
-    try {
-      options = {
-	key:  fs.readFileSync(PRIVKEY_PATH),
-	cert: fs.readFileSync(CERT_PATH)
-      };
-      // TODO(Frazier): remove asap, security vulnerability
-      process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-    } catch(error) {
-      console.log({ error })
-    }
+  const { PRIVKEY_PATH, CERT_PATH } = process.env
+  try {
+    options = {
+      key:  fs.readFileSync(PRIVKEY_PATH),
+      cert: fs.readFileSync(CERT_PATH)
+    };
+    // TODO(Frazier): remove asap, security vulnerability
+    process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+  } catch(error) {
+    console.log({ error })
   }
 
   let server
