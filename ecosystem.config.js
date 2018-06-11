038c7be73765d044df33287b2d88f676776cfc9f
Jonathan Frazier <jjfraz11@gmail.com>
2018-06-11 10:45:17 -0700

    Add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'mochiapp',
+    script: './server/src/index.js'
+  }],
+  deploy: {
+    staging: {
+      user: 'ubuntu',
+      host: 'ec2-18-216-197-134.us-east-2.compute.amazonaws.com',
+      key: '~/.ssh/mochiserver.pem',
+      ref: 'origin/server.setup',
+      repo: 'git@github.com:mochiapp/mochi.git',
+      path: '/home/ubuntu/projects/mochi',
+      'post-deploy': 'yarn && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
