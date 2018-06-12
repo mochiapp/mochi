58acbc78202dde2cc88e3256cfadabf83f7a66a5
Jonathan Frazier <jjfraz11@gmail.com>
2018-06-11 18:38:44 -0700

    Add server config to pm2.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,16 @@ module.exports = {
     script: './server/src/index.js'
   }],
   deploy: {
+    production: {
+      user: 'root',
+      host: '149.28.235.44',
+      key: '~/.ssh/id_rsa.pub',
+      port: '2022',
+      ref: 'origin/server.setup',
+      repo: 'git@github.com:mochiapp/mochi.git',
+      path: '/var/www/mochi.social',
+      'post-deploy': 'git checkout server.setup && cd app/ && yarn && quasar build && cd ../server/ && yarn && pm2 startOrRestart ../ecosystem.config.js'
+    },
     staging: {
       user: 'ubuntu',
       host: 'ec2-18-216-197-134.us-east-2.compute.amazonaws.com',
@@ -11,7 +21,7 @@ module.exports = {
       ref: 'origin/server.setup',
       repo: 'git@github.com:mochiapp/mochi.git',
       path: '/home/ubuntu/projects/mochi',
-      'post-deploy': 'cd app/ && yarn && quasar build && cd ../server/ && yarn && pm2 startOrRestart ../ecosystem.config.js'
+      'post-deploy': 'git checkout server.setup && cd app/ && yarn && quasar build && cd ../server/ && yarn && pm2 startOrRestart ../ecosystem.config.js'
     }
   }
 }
