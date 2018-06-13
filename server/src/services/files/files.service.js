9c35b2e2039f6301273af1d383463070438baf01
Jonathan Frazier <jjfraz11@gmail.com>
2018-06-13 16:07:14 -0700

    Add blob-store and feathers-blob service.

--- server/src/services/files/files.service.js
+++ server/src/services/files/files.service.js
@@ -2,6 +2,10 @@
 const createService = require('./files.class.js');
 const hooks = require('./files.hooks');
 
+// Reference: https://github.com/feathersjs/docs/blob/master/guides/advanced/file-uploading.md
+const blobService = require('feathers-blob')
+const blobStorage = require('fs-blob-store')('./uploads')
+
 module.exports = function (app) {
   
   const paginate = app.get('paginate');
@@ -11,7 +15,8 @@ module.exports = function (app) {
   };
 
   // Initialize our service with any options it requires
-  app.use('/api/files', createService(options));
+  app.use('/api/files', createService(options))
+  app.use('/api/files-multi', blobService({ Model: blobStorage }))
 
   // Get our initialized service so that we can register hooks
   const service = app.service('api/files');
