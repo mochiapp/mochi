a23d28aa09272533af15bf74c98aeafcece2715a
Jonathan Frazier <jjfraz11@gmail.com>
2018-06-14 11:37:11 -0700

    Use staging branch for deploys.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -18,10 +18,10 @@ module.exports = {
       user: 'ubuntu',
       host: 'ec2-18-216-197-134.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/mochiserver.pem',
-      ref: 'origin/server.setup',
+      ref: 'origin/staging',
       repo: 'git@github.com:mochiapp/mochi.git',
       path: '/home/ubuntu/projects/mochi',
-      'post-deploy': 'git checkout server.setup && cd app/ && yarn && quasar build && cd ../server/ && yarn && pm2 startOrRestart ../ecosystem.config.js'
+      'post-deploy': 'git checkout staging && cd app/ && yarn && quasar build && cd ../server/ && yarn && pm2 startOrRestart ../ecosystem.config.js'
     }
   }
 }
